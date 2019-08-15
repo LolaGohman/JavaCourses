e6881356d6bdc003c5402686da7226ccb006f863
LolaGohman <48091762+LolaGohman@users.noreply.github.com>
2019-08-15 22:21:11 +0300

    Delete StringRepeaterStringBufferImpl.java

--- Lesson02/src/main/java/org/courses/lesson2/StringRepeaterStringBufferImpl.java
+++
@@ -1,8 +0,0 @@
-package org.courses.lesson2;
-
-public class StringRepeaterStringBufferImpl extends StringRepeaterStringAppendImpl {
-    @Override
-    public String repeat(String string, int times) {
-        return super.repeat(string, times);
-    }
-}
