dd9be4b45d58bf8026828989335ee8feb7965b2c
LolaGohman <48091762+LolaGohman@users.noreply.github.com>
2019-08-15 22:26:06 +0300

    Delete StringRepeaterStringConcatImpl.java

--- Lesson02/src/main/java/org/courses/lesson2/StringRepeaterStringConcatImpl.java
+++
@@ -1,8 +0,0 @@
-package org.courses.lesson2;
-
-public class StringRepeaterStringConcatImpl extends StringRepeaterStringAppendImpl {
-    @Override
-    public String repeat(String string, int times) {
-        return super.repeat(string, times);
-    }
-}
