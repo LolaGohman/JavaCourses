19d883715c32f60441bb3e5d4b9197194ed2e431
LolaGohman <48091762+LolaGohman@users.noreply.github.com>
2019-08-15 22:21:20 +0300

    Delete StringRepeaterStringBuilderImpl.java

--- Lesson02/src/main/java/org/courses/lesson2/StringRepeaterStringBuilderImpl.java
+++
@@ -1,8 +0,0 @@
-package org.courses.lesson2;
-
-public class StringRepeaterStringBuilderImpl extends StringRepeaterStringAppendImpl {
-    @Override
-    public String repeat(String string, int times) {
-        return super.repeat(string, times);
-    }
-}
